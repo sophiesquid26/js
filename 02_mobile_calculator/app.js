6abb5c9a7a1e8be6c96aec69250dc536f26b481d
sophiesquid26 <146354133+sophiesquid26@users.noreply.github.com>
2023-11-11 12:58:46 +0800

    apply calculations

--- 02_mobile_calculator/app.js
+++ 02_mobile_calculator/app.js
@@ -1,13 +1,52 @@
 const screen =document.querySelector("#screen")
 const numBtns = document.querySelector("button.num")
+const oprBtns = document.querySelector("button.opr")
+const calBtns = document.querySelector("button.cal")
+
 
 let a = 0
+let operation = null
+let b = 0
 
 for(let i = 0; i < numBtns.length; i++) {
     const numBtn = numBtns[i]
 
     numBtn.addEventListener("click", function onBtnClick() {
-        a = a + numBtn.dataset.num
-        screen.innerHTML = a
+        if (operation !== null) {
+            b = b + numBtn.dataset.num
+            screen.innerHTML = + b
+        } else {
+            a = a + numBtn.dataset.num
+            screen.innerHTML = + a
+        }
     })
 }
+
+for(let i = 0; i < oprBtns.legth; i++) {
+    const oprBtn = oprBtns[i]
+
+    oprBtn.addEventListener("click", function () {
+        operation = oprBtn.dataset.opr
+    })
+    
+}
+
+calBtns.addEventListener("click", function() {
+    switch(operation){
+        case "+":
+            screen.innerHTML = a + b
+            break
+
+        case "-":
+            screen.innerHTML = a - b
+            break
+
+        case "*":
+            screen.innerHTML = a * b
+            break
+    
+        case "/":
+            screen.innerHTML = a / b
+            break
+    }   
+})
